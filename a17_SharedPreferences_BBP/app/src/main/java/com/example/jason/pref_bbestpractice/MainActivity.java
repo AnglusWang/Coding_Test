2e977f719a8ab972677a46d78dbf73599ff67084
JesonMe <1655087079@qq.com>
2016-04-12 21:53:29 +0800

    Tortoise commit

---
+++ a17_SharedPreferences_BBP/app/src/main/java/com/example/jason/pref_bbestpractice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jason.pref_bbestpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
