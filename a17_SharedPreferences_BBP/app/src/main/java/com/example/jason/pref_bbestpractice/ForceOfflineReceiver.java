e0183cee8dccf9b1c196dd72cdab96f39a51c2fc
Jeson <1655087079@qq.com>
2016-04-15 22:42:43 +0800

    修复bug 可以成功跳转

--- a17_SharedPreferences_BBP/app/src/main/java/com/example/jason/pref_bbestpractice/ForceOfflineReceiver.java
+++ a17_SharedPreferences_BBP/app/src/main/java/com/example/jason/pref_bbestpractice/ForceOfflineReceiver.java
@@ -19,16 +19,16 @@ public class ForceOfflineReceiver extends BroadcastReceiver{
         Toast.makeText(context, "接收到广播", Toast.LENGTH_SHORT).show();
 
         AlertDialog.Builder dialogBuilder =  new AlertDialog.Builder(context);
-        dialogBuilder.setTitle("Warning");
-        dialogBuilder.setMessage("You are forced to be offline." +
-                " Please try to login again.");
+        dialogBuilder.setTitle("下线通知");
+        dialogBuilder.setMessage("你的账户在其他地方登录 Over");
         dialogBuilder.setCancelable(false);
-        dialogBuilder.setPositiveButton("OK",
+        dialogBuilder.setPositiveButton("重新登录",
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         ActivityCollector.finishAll();//摧毁所有活动
                         Intent intent1 = new Intent(context, LoginActivity.class);
+                        //在广播接受者中启动活动需要添加FLAG_ACTIVITY_NEW_TASK标志
                         intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         context.startActivity(intent1);
                     }
@@ -36,6 +36,5 @@ public class ForceOfflineReceiver extends BroadcastReceiver{
         AlertDialog alertDialog = dialogBuilder.create();
         alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
         alertDialog.show();
-        dialogBuilder.show();
     }
 }
