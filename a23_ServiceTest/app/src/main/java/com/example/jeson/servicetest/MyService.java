5a6ef5e3055c8468625fb3ace281451c0017b134
Jeson <1655087079@qq.com>
2016-04-19 21:28:00 +0800

    创建了一个服务 完成启动和停止服务演示

--- a23_ServiceTest/app/src/main/java/com/example/jeson/servicetest/MyService.java
+++ a23_ServiceTest/app/src/main/java/com/example/jeson/servicetest/MyService.java
@@ -3,14 +3,15 @@ package com.example.jeson.servicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by Jeson on 2016/4/19.
  * 服务类
+ * 调用自身的stopSelf方法就能让这个服务停止下来了
  */
 public class MyService extends Service {
-    @Nullable
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -18,16 +19,19 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MyService", "onCreate executed");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
     }
 }
