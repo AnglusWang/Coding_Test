cf6c1cd5cc7140533fec1ba4597dcd86be68c9ff
AnglusWang <AnglusWang@163.com>
2016-06-11 15:39:44 +0800

    一个超级设备管理器-----激活后，可以进行一键锁屏、清除数据操作

---
+++ a27_aKeyLockScreen/app/src/main/java/com/example/angluswang/akeylockscreen/AdminReceiver.java
@@ -0,0 +1,12 @@
+package com.example.angluswang.akeylockscreen;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by Jeson on 2016/6/11.
+ *  管理员接收者
+ */
+
+public class AdminReceiver extends DeviceAdminReceiver {
+
+}
