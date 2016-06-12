bf55107044fd06b4030e9bddf3f50a67f70d577b
AnglusWang <AnglusWang@163.com>
2016-06-12 22:57:11 +0800

    新建一个类GetNumber，用于获取手机通讯录中的联系人姓名和电话号码

---
+++ a_28Contacts/app/src/main/java/com/example/angluswang/contacts/GetNumber.java
@@ -0,0 +1,32 @@
+package com.example.angluswang.contacts;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+
+/**
+ * Created by Jeson on 2016/6/12.
+ * 获取号码
+ */
+
+public class GetNumber {
+
+    public static String getNumber(Context context) {
+        Cursor cursor = context.getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, null);
+        String phoneNumber;
+        String phoneName;
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                phoneNumber = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER));
+                phoneName = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                System.out.println("联系人：" + phoneName + "; 号码：" + phoneNumber);
+            }
+            cursor.close();
+        }
+        return null;
+    }
+}
