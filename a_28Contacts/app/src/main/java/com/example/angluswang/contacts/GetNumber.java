d9bff6c71bcb8c6106709c7daf77b071ed3814a8
AnglusWang <AnglusWang@163.com>
2016-06-12 23:03:47 +0800

    将数据添加到集合List<PhoneInfo>中

--- a_28Contacts/app/src/main/java/com/example/angluswang/contacts/GetNumber.java
+++ a_28Contacts/app/src/main/java/com/example/angluswang/contacts/GetNumber.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jeson on 2016/6/12.
  * 获取号码
  */
 
 public class GetNumber {
+
+    public static List<PhoneInfo> sList = new ArrayList<>();
 
     public static String getNumber(Context context) {
         Cursor cursor = context.getContentResolver().query(
@@ -24,6 +29,9 @@ public class GetNumber {
                 phoneName = cursor.getString(cursor.getColumnIndex(
                         ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                 System.out.println("联系人：" + phoneName + "; 号码：" + phoneNumber);
+
+                PhoneInfo phoneInfo = new PhoneInfo(phoneName, phoneNumber);
+                sList.add(phoneInfo);
             }
             cursor.close();
         }
