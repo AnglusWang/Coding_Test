c3595f52c092968009e9a8c221f1204b1f0ab94e
AnglusWang <AnglusWang@163.com>
2016-06-13 09:19:49 +0800

    优化自定义的适配器，采用ViewHolder来进行优化

--- a_28Contacts/app/src/main/java/com/example/angluswang/contacts/MyAdapter.java
+++ a_28Contacts/app/src/main/java/com/example/angluswang/contacts/MyAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -19,7 +18,6 @@ public class MyAdapter extends BaseAdapter {
 
     private List<PhoneInfo> mList;
     private Context mContext;
-    private LinearLayout mLayout;
 
     public MyAdapter(List<PhoneInfo> list, Context context) {
         mList = list;
@@ -44,13 +42,33 @@ public class MyAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        mLayout = (LinearLayout) inflater.inflate(R.layout.item_lvcontact, null);
-        TextView phoneName = (TextView) mLayout.findViewById(R.id.tv_contact_name);
-        TextView phoneNumber = (TextView) mLayout.findViewById(R.id.tv_contact_number);
-        phoneName.setText(mList.get(position).getName());
-        phoneNumber.setText(mList.get(position).getNumber());
+//        LayoutInflater inflater = LayoutInflater.from(mContext);
+//        mLayout = (LinearLayout) inflater.inflate(R.layout.item_lvcontact, null);
+//        TextView phoneName = (TextView) mLayout.findViewById(R.id.tv_contact_name);
+//        TextView phoneNumber = (TextView) mLayout.findViewById(R.id.tv_contact_number);
+//        phoneName.setText(mList.get(position).getName());
+//        phoneNumber.setText(mList.get(position).getNumber());
 
-        return mLayout;
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_lvcontact, null);
+            holder = new ViewHolder();
+            holder.phoneName = (TextView) convertView.findViewById(R.id.tv_contact_name);
+            holder.phoneNumber = (TextView) convertView.findViewById(R.id.tv_contact_number);
+            holder.phoneName.setText(mList.get(position).getName());
+            holder.phoneNumber.setText(mList.get(position).getNumber());
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+            holder.phoneName.setText(mList.get(position).getName());
+            holder.phoneNumber.setText(mList.get(position).getNumber());
+        }
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView phoneName;
+        TextView phoneNumber;
     }
 }
