3114a4627b869e66c1f69ef4339bd7476c729304
AnglusWang <AnglusWang@163.com>
2016-06-22 18:09:07 +0800

    通过ViewDragHelper 实现QQ侧滑菜单效果（存在bug） --- NullPointerException：Attempt to invoke virtual method 'int android.view.View.getMeasuredWidth()' ...

---
+++ a30_DragViewTest/app/src/main/java/com/angluswang/dragviewtest/DragViewGroup.java
@@ -0,0 +1,120 @@
+package com.angluswang.dragviewtest;
+
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Jeson on 2016/6/22.
+ * 通过ViewDragHelper 实现QQ侧滑功能
+ */
+
+public class DragViewGroup extends FrameLayout {
+
+    private ViewDragHelper mViewDragHelper;
+    private View mMenuView, mMainView;
+    private int mWidth;
+
+    public DragViewGroup(Context context) {
+        this(context, null);
+    }
+
+    public DragViewGroup(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mMenuView = getChildAt(0);
+        mMainView = getChildAt(1);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = mMenuView.getMeasuredWidth();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return mViewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //将触摸事件传递给ViewDragHelper 此操作必不可少
+
+        mViewDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    private void initView() {
+        mViewDragHelper = ViewDragHelper.create(this, callback);
+    }
+
+    private ViewDragHelper.Callback callback =
+            new ViewDragHelper.Callback() {
+                //何时开始检测触摸事件
+                @Override
+                public boolean tryCaptureView(View child, int pointerId) {
+                    //如果当前触摸的child是mMainView时开始检测
+                    return mMainView == child;
+                }
+
+                //处理垂直滑动
+                @Override
+                public int clampViewPositionVertical(View child, int top, int dy) {
+                    return 0;
+                }
+
+                //处理水平滑动
+                @Override
+                public int clampViewPositionHorizontal(View child, int left, int dx) {
+                    return left;
+                }
+
+                //拖动结束后调用
+                @Override
+                public void onViewReleased(View releasedChild, float xvel, float yvel) {
+                    super.onViewReleased(releasedChild, xvel, yvel);
+                    //手指抬起后缓慢移动到指定距离
+                    if (mMainView.getLeft() < 500) {
+                        //关闭菜单
+                        //相当于 Scroller 的 startScroll 方法
+                        mViewDragHelper.smoothSlideViewTo(mMainView, 0, 0);
+                        ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
+                    } else {
+                        //打开菜单
+                        mViewDragHelper.smoothSlideViewTo(mMainView, 300, 0);
+                        ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
+                    }
+                }
+            };
+
+    @Override
+    public void computeScroll() {
+        if (mViewDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    /**
+     * 在ViewDragHelper.Callback中，系统定义了大量的监听事件来帮助我们处理各种事件，如下：
+     * onViewCaptured():
+     *      这个事件在用户触摸到View后回调
+     * onViewDragStateChanged():
+     *      这个事件在拖拽状态改变时回调，比如idle， dragging等状态
+     * onViewPositionChanged():
+     *      这个事件在位置改变时回调，常用于滑动时更改scale 进行缩放等效果
+     */
+}
