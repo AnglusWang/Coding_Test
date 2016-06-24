206654b6ba029d635f363ada75f1dfe89d51da2c
AnglusWang <AnglusWang@163.com>
2016-06-24 09:19:00 +0800

    实现界面切换

---
+++ zhy_weixin/app/src/main/java/com/angluswang/zhy_weixin/ContactTabFragment.java
@@ -0,0 +1,21 @@
+package com.angluswang.zhy_weixin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jeson on 2016/6/24.
+ * 聊天界面窗口
+ */
+
+public class ContactTabFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab3, container, false);
+    }
+}
--- zhy_weixin/app/src/main/java/com/angluswang/zhy_weixin/MainActivity.java
+++ zhy_weixin/app/src/main/java/com/angluswang/zhy_weixin/MainActivity.java
@@ -1,13 +1,50 @@
 package com.angluswang.zhy_weixin;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private List<Fragment> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        mViewPager.setAdapter(mAdapter);
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        mDatas = new ArrayList<>();
+        ChatMainTabFragment tab1 = new ChatMainTabFragment();
+        FoundTabFragment tab2 = new FoundTabFragment();
+        ContactTabFragment tab3 = new ContactTabFragment();
+        mDatas.add(tab1);
+        mDatas.add(tab2);
+        mDatas.add(tab3);
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mDatas.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mDatas.size();
+            }
+        };
     }
 }
