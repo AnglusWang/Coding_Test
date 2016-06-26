244b623d58b6ac9ae1786ca6730ed28e9b81e64c
AnglusWang <AnglusWang@163.com>
2016-06-26 16:11:06 +0800

    完成自定义属性的编写

--- ArcMenu/app/src/main/java/com/angluswang/arcmenu/ArcMenu.java
+++ ArcMenu/app/src/main/java/com/angluswang/arcmenu/ArcMenu.java
@@ -1,7 +1,10 @@
 package com.angluswang.arcmenu;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -10,6 +13,39 @@ import android.view.ViewGroup;
  */
 
 public class ArcMenu extends ViewGroup {
+
+    private static final int POS_LEFT_TOP = 0;
+    private static final int POS_LEFT_BOTTOM = 1;
+    private static final int POS_RIGHT_TOP = 2;
+    private static final int POS_RIGHT_BOTTOM = 3;
+
+    private Position mPosition = Position.RIGHT_BOTTOM;
+    private int mRadius;
+
+    private Status mMenuStatus = Status.CLOSE;
+    private OnMenuItemClickListener mOnMenuItemClickListener;
+
+    // 菜单的主按钮
+    private View mCButton;
+
+    // 菜单的状态
+    private enum Status {
+        CLOSE, OPEN
+    }
+
+    // 菜单的位置，枚举类
+    private enum Position {
+        LEFT_TOP, LEFT_BOTTOM, RIGHT_TOP, RIGHT_BOTTOM
+    }
+
+    // 点击子菜单项的回调接口
+    public interface OnMenuItemClickListener {
+        void onClick(View view, int position);
+    }
+
+    public void setOnMenuItemClickListener(OnMenuItemClickListener onMenuItemClickListener) {
+        mOnMenuItemClickListener = onMenuItemClickListener;
+    }
 
     public ArcMenu(Context context) {
         this(context, null);
@@ -21,6 +57,37 @@ public class ArcMenu extends ViewGroup {
 
     public ArcMenu(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        mRadius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                100, getResources().getDisplayMetrics());
+        //获取自定义属性的值
+        TypedArray ta = context.getTheme().obtainStyledAttributes(
+                attrs, R.styleable.ArcMenu, defStyleAttr, 0);
+
+        int pos = ta.getInt(R.styleable.ArcMenu_position, POS_RIGHT_BOTTOM);
+        switch (pos) {
+            case POS_LEFT_TOP:
+                mPosition = Position.LEFT_TOP;
+                break;
+            case POS_LEFT_BOTTOM:
+                mPosition = Position.LEFT_BOTTOM;
+                break;
+            case POS_RIGHT_TOP:
+                mPosition = Position.RIGHT_TOP;
+                break;
+            case POS_RIGHT_BOTTOM:
+                mPosition = Position.RIGHT_BOTTOM;
+                break;
+            default:
+                break;
+        }
+        mRadius = (int) ta.getDimension(R.styleable.ArcMenu_radius,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100,
+                        getResources().getDisplayMetrics()));
+
+        System.out.println("mPosition: " + mPosition + "; mRadius: " + mRadius);
+
+        ta.recycle();
     }
 
     @Override
