55b40ee589afb95457ef76dac5e9454ec60b4fe8
AnglusWang <AnglusWang@163.com>
2016-07-05 12:00:59 +0800

    制作全新的TitleBar

--- weixin_60_mainlayout/app/src/main/java/com/angluswang/weixin_60_mainlayout/MainActivity.java
+++ weixin_60_mainlayout/app/src/main/java/com/angluswang/weixin_60_mainlayout/MainActivity.java
@@ -2,6 +2,12 @@ package com.angluswang.weixin_60_mainlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.ViewConfiguration;
+import android.view.Window;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +15,52 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setOverflowButtonAlways();
+        getActionBar().setDisplayShowHomeEnabled(false); // 隐藏微信左边的图标
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * 设置 把actionbar的（默认...）显示出来
+     */
+    private void setOverflowButtonAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKey = ViewConfiguration.class
+                    .getDeclaredField("sHasPermanentMenuKey");
+            menuKey.setAccessible(true);
+            menuKey.setBoolean(config, false); // 换成true可看到效果 ， 4.1 默认为false
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 设置menu显示icon
+     */
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return super.onMenuOpened(featureId, menu);
     }
 }
