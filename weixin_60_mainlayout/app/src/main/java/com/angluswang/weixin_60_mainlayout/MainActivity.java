24ccc5756bfc5b4b80fececb785ff37af6db0770
AnglusWang <AnglusWang@163.com>
2016-07-05 09:29:01 +0800

    新建一个 高仿微信6.0主界面的 项目

---
+++ weixin_60_mainlayout/app/src/main/java/com/angluswang/weixin_60_mainlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.angluswang.weixin_60_mainlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
