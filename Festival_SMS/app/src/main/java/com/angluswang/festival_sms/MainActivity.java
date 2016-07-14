c9111a02836d9be996a18c71b09d5d93e1de3216
AnglusWang <AnglusWang@163.com>
2016-07-14 23:09:16 +0800

    UI 框架搭建

--- Festival_SMS/app/src/main/java/com/angluswang/festival_sms/MainActivity.java
+++ Festival_SMS/app/src/main/java/com/angluswang/festival_sms/MainActivity.java
@@ -1,13 +1,47 @@
 package com.angluswang.festival_sms;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+    private String[] mTitles = new String[]{"节日短信", "发送记录"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        mTabLayout = (TabLayout) findViewById(R.id.id_tablayout);
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return new FestivalCategoryFragment();
+            }
+
+            @Override
+            public int getCount() {
+                return mTitles.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitles[position];
+            }
+        });
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
