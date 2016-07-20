dc7b2da0f30fa343b34854bbab68646e0cbdac1b
AnglusWang <AnglusWang@163.com>
2016-07-20 14:55:35 +0800

    增删改查 ContentProvider

--- Festival_SMS/app/src/main/java/com/angluswang/festival_sms/biz/SmsBiz.java
+++ Festival_SMS/app/src/main/java/com/angluswang/festival_sms/biz/SmsBiz.java
@@ -1,9 +1,15 @@
 package com.angluswang.festival_sms.biz;
 
 import android.app.PendingIntent;
+import android.content.ContentValues;
+import android.content.Context;
 import android.telephony.SmsManager;
 
+import com.angluswang.festival_sms.bean.SendedMsg;
+import com.angluswang.festival_sms.db.SmsProvider;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Set;
 
 /**
@@ -11,6 +17,12 @@ import java.util.Set;
  * 短信业务
  */
 public class SmsBiz {
+
+    private Context context;
+
+    public SmsBiz(Context context) {
+        this.context = context;
+    }
 
     public int sendMsg(String number, String msg,
                        PendingIntent sentPi, PendingIntent deliverPi) {
@@ -22,13 +34,30 @@ public class SmsBiz {
         return contents.size();
     }
 
-    public int sendMsg(Set<String> numbers, String msg,
+    public int sendMsg(Set<String> numbers, SendedMsg msg,
                        PendingIntent sentPi, PendingIntent deliverPi) {
+        save(msg);
         int result = 0;
         for (String number : numbers) {
-            int count = sendMsg(number, msg, sentPi, deliverPi);
+            int count = sendMsg(number, msg.getMsg(), sentPi, deliverPi);
             result += count;
         }
         return result;
+    }
+
+    /**
+     * 将发送的短信相关信息保存到本地数据库中
+     * @param sendedMsg
+     */
+    private void save(SendedMsg sendedMsg) {
+        sendedMsg.setDate(new Date());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(SendedMsg.COLUME_DATE, sendedMsg.getDate().getTime());
+        contentValues.put(SendedMsg.COLUME_FESTIVAL_NAME, sendedMsg.getFestivalName());
+        contentValues.put(SendedMsg.COLUME_MSG, sendedMsg.getMsg());
+        contentValues.put(SendedMsg.COLUME_NAMES, sendedMsg.getNames());
+        contentValues.put(SendedMsg.COLUME_NUMBERS, sendedMsg.getNumbers());
+
+        context.getContentResolver().insert(SmsProvider.URI_SMS_ALL, contentValues);
     }
 }
---
+++ Festival_SMS/app/src/main/java/com/angluswang/festival_sms/db/SmsProvider.java
@@ -0,0 +1,100 @@
+package com.angluswang.festival_sms.db;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import com.angluswang.festival_sms.bean.SendedMsg;
+
+/**
+ * Created by Jeson on 2016/7/20.
+ * SMSProvider 数据提供者
+ */
+
+public class SmsProvider extends ContentProvider {
+
+    private static final String AUTHORITY = "com.angluswang.sms.provider.SmsProvider";
+    public static final Uri URI_SMS_ALL = Uri.parse("content://" + AUTHORITY + "/sms");
+
+    private static UriMatcher sMatcher;
+
+    private static final int SMS_ALL = 0;
+    private static final int SMS_ONE = 1;
+
+    static {
+        sMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        sMatcher.addURI(AUTHORITY, "sms", SMS_ALL);
+        sMatcher.addURI(AUTHORITY, "sms/#", SMS_ONE);
+    }
+
+    private SmsDbOpenHelper mHelper;
+    private SQLiteDatabase mDatabase;
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        int match = sMatcher.match(uri);
+        switch (match) {
+            case SMS_ALL:
+                break;
+            case SMS_ONE:
+                long id = ContentUris.parseId(uri);
+                s = "_id = ?";
+                strings1 = new String[]{String.valueOf(id)};
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong URI : " + uri);
+        }
+        mDatabase = mHelper.getReadableDatabase();
+        Cursor cursor = mDatabase.query(SendedMsg.TABLE_NAME, strings, s, strings1, null, null, s1);
+
+        cursor.setNotificationUri(getContext().getContentResolver(), URI_SMS_ALL);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        int match = sMatcher.match(uri);
+        if (match != SMS_ALL) {
+            throw new IllegalArgumentException("Wrong URI : " + uri);
+        }
+        mDatabase = mHelper.getWritableDatabase();
+        long rowId = mDatabase.insert(SendedMsg.TABLE_NAME, null, contentValues);
+        if (rowId > 0) {
+            notifyDataSetChanged();
+            return ContentUris.withAppendedId(uri, rowId);
+        }
+        return null;
+    }
+
+    private void notifyDataSetChanged() {
+        getContext().getContentResolver().notifyChange(URI_SMS_ALL, null);
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
