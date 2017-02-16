3185fcf415a6e4c86df19e6a1f02681c82c6331b
AnglusWang <angluswang@gmail.com>
2017-02-16 15:16:11 +0800

    [test] - 使用 Canvas 的 drawBitmap 方法制作简单的动画效果图案

--- CanvasDemo/app/src/main/java/com/angluswang/canvasdemo/MainActivity.java
+++ CanvasDemo/app/src/main/java/com/angluswang/canvasdemo/MainActivity.java
@@ -2,13 +2,33 @@ package com.angluswang.canvasdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    CheckView cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cv = (CheckView) findViewById(R.id.id_checkView);
+        Button check = (Button) findViewById(R.id.btn_check);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cv.check();
+            }
+        });
+        Button unCheck = (Button) findViewById(R.id.btn_unCheck);
+        unCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cv.unCheck();
+            }
+        });
 
 //        ArrayList<PieData> datas = new ArrayList();
 //        PieData pieData = new PieData("name1", 15.0f);
